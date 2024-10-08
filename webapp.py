633269736258b9d19d70e85cdf5905ac3bbfbfdd
danyewest97 <dane20052008@gmail.com>
2024-10-08 13:04:08 -0700

    quick commit

--- webapp.py
+++ webapp.py
@@ -20,7 +20,27 @@ def render_fact():
     county2 = county_most_women(state)
     fact = "In " + state + ", the county with the highest percentage of under 18 year olds is " + county + "."
     fact2 = "In " + state + ", the county with the highest percentage of women is " + county2 + "."
-    return render_template('home.html', states=states, funFact=fact, funFact2=fact2)
+    fact3 = request.args.get('fact3')
+    if fact3 == None:
+        fact3 = ""
+    return render_template('home.html', states=states, funFact=fact, funFact2=fact2, funFact3=fact3)
+    
+@app.route('/secondFact')
+def render_second_fact():
+    states = get_states()
+    state = request.args.get('state2')
+    lowest_bachelors = county_lowest_bachelors(state)
+    county = lowest_bachelors[0]
+    percent = lowest_bachelors[1]
+    fact = request.args.get('fact')
+    fact2 = request.args.get('fact2')
+    
+    if percent is not None:
+        fact3 = "In " + state + ", the county with the lowest percentage of people with a bachelor's degree or higher is " + county + ", with only " + str(percent) + " percent of people having a bachelor's degree or higher."
+    else:
+        fact3 = ""
+    
+    return render_template('home.html', states=states, funFact=fact, funFact2=fact2, funFact3=fact3)
     
 def get_states():
     """Return a list of state abbreviations from the demographic data."""
@@ -48,7 +68,6 @@ def county_most_under_18(state):
     return county
 
 def county_most_women(state):
-    """Return the name of a county in the given state with the highest percent of under 18 year olds."""
     with open('demographics.json') as demographics_data:
         counties = json.load(demographics_data)
     highest=0
@@ -59,6 +78,19 @@ def county_most_women(state):
                 highest = c["Miscellaneous"]["Percent Female"]
                 county = c["County"]
     return county
+    
+def county_lowest_bachelors(state):
+    with open('demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
+    lowest=100
+    county = ""
+    for c in counties:
+        if c["State"] == state:
+            if c["Education"]["Bachelor's Degree or Higher"] < lowest:
+                lowest = c["Education"]["Bachelor's Degree or Higher"]
+                county = c["County"]
+    result = [county, lowest]
+    return result
 
 def is_localhost():
     """ Determines if app is running on localhost or not
